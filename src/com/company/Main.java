b0dcc16b44e7b98f219b9ecc86429c46a4a72ffd
Java Courses 1 <JavaCourses1@tti.lv>
2020-02-06 18:41:51 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.println("What is your name?");
+        String name = scan.next();
+        System.out.println("Hello, " + name);
+
+        int myNum = rand.nextInt(100) + 1;
+        System.out.println("Cheat: " + myNum);
+
+        for (int i = 0; i < 10; i = i + 1) {
+            System.out.println("Enter your guess");
+            int userNum = scan.nextInt();
+
+            if (myNum == userNum) {
+                System.out.println("You win!");
+                break;
+            }
+
+        }
+
+    }
+}
